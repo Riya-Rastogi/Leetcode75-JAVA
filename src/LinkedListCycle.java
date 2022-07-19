6b1bc2379d77e858f917e032524d3d8470607a9c
Riya-Rastogi <riyarastogi938@gmail.com>
2022-07-19 22:00:54 +0530

    leetcode challanges

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class LinkedListCycle {
+        public ListNode detectCycle(ListNode head) {
+
+            HashSet<ListNode> set = new HashSet<>();
+            ListNode temp = head;
+            while(temp != null){
+                if(set.contains(temp)) return temp;
+                set.add(temp);
+                temp = temp.next;
+            }
+            return null;
+        }
+    }
+
+
